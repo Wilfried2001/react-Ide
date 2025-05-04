4453778f403fef430a2a1dd4878ce3735053413b
Wilfried2001 <wizzypump@gmail.com>
2025-05-04 02:33:32 +0200

    first commit

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import CodeTab from "./CodeTab";
+import { useSelector, useDispatch } from "react-redux";
+import ButtonTab from "./ButtonTab";
+import Preview from "./Preview";
+
+export default function Tabs() {
+  const tabs = useSelector((state) => state.tabs);
+  const PreviewData = useSelector((state) => state.preview);
+  const [tabIndex, setTabIndex] = useState(tabs[0].id);
+  return (
+    <div className="flex grow">
+      <div className="grow flex flex-col w-[175px] shrink-0 text-slate-300 border-r border-slate-200">
+        {tabs.map((tab) => (
+          <ButtonTab
+            key={tab.id}
+            id={tab.id}
+            toggleTab={(id) => setTabIndex(id)}
+            imgURL={tab.imgURL}
+            buttonContent={tab.buttonContent}
+          />
+        ))}
+      </div>
+      <div className="w-full grow relative">
+        <CodeTab
+          id={tabIndex}
+          code={tabs.find((obj) => obj.id === tabIndex).code}
+        /> {PreviewData.preview && <Preview />}
+      </div>
+    </div>
+  );
+}
